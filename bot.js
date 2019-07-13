40b2b8e6b93316314bd17750a9d6f44356b1d8c2
DHNoud <43524733+DHNoud@users.noreply.github.com>
2019-07-13 13:41:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -411,6 +411,31 @@ client.on('message', message => {
 });
 
 
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("4898415217393008740);
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("355724970976083972");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;        
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
 
 
 client.login(process.env.BOT_TOKEN);
